468a55f231433df1bfa51e90beed1ca98564d88d
Zhigang Chen <zhigang.chen@moovweb.com>
2012-03-02 02:01:34 -0800

    add two test cases: one for 1800flowers and another for cancer

--- detect_test.go
+++ detect_test.go
@@ -34,3 +34,37 @@ func TestDetectLatin1(t *testing.T) {
 	}
 	detector.Free()
 }
+
+func TestDetectCp1252(t *testing.T) {
+	data, err := ioutil.ReadFile("input_cp1252.html")
+	if err != nil {
+		t.Errorf("error: %s", err.String())
+	}
+	detector, err := NewCharsetDetector()
+	if err != nil {
+		t.Errorf("error: %s", err.String())
+	}
+	charset := detector.GuessCharset(data)
+	if charset != "windows-1252" {
+		println(charset)
+		t.Errorf("the charset guess was wrong")
+	}
+	detector.Free()
+}
+
+func TestDetectUtf8(t *testing.T) {
+	data, err := ioutil.ReadFile("input_utf8.html")
+	if err != nil {
+		t.Errorf("error: %s", err.String())
+	}
+	detector, err := NewCharsetDetector()
+	if err != nil {
+		t.Errorf("error: %s", err.String())
+	}
+	charset := detector.GuessCharset(data)
+	if charset != "UTF-8" {
+		println(charset)
+		t.Errorf("the charset guess was wrong")
+	}
+	detector.Free()
+}
