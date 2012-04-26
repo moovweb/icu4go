7e7de299173446cbe0a2c79b207bab188489b57b
Aaron Leung <aaron.leung@moovweb.com>
2012-04-25 17:28:38 -0700

    Tests for the broken encoding detection.

--- detect_test.go
+++ detect_test.go
@@ -3,8 +3,24 @@ package icu4go
 import "testing"
 import "io/ioutil"
 
-func TestDetectChinese(t *testing.T) {
-	data, err := ioutil.ReadFile("input_cn.html")
+func _TestDetectChinese(t *testing.T) {
+ data, err := ioutil.ReadFile("input_cn.html")
+ if err != nil {
+   t.Errorf("error: %s", err.Error())
+ }
+ detector, err := NewCharsetDetector()
+ if err != nil {
+   t.Errorf("error: %s", err.Error())
+ }
+ charset := detector.GuessCharset(data)
+ if charset != "GB18030" {
+   t.Errorf("the charset guess was wrong")
+ }
+ detector.Free()
+}
+
+func TestDetectLatin1(t *testing.T) {
+	data, err := ioutil.ReadFile("input_latin1.html")
 	if err != nil {
 		t.Errorf("error: %s", err.Error())
 	}
@@ -13,14 +29,14 @@ func TestDetectChinese(t *testing.T) {
 		t.Errorf("error: %s", err.Error())
 	}
 	charset := detector.GuessCharset(data)
-	if charset != "GB18030" {
+	if charset != "ISO-8859-1" {
 		t.Errorf("the charset guess was wrong")
 	}
 	detector.Free()
 }
 
-func TestDetectLatin1(t *testing.T) {
-	data, err := ioutil.ReadFile("input_latin1.html")
+func TestDetectHammacher(t *testing.T) {
+	data, err := ioutil.ReadFile("hammacher.html")
 	if err != nil {
 		t.Errorf("error: %s", err.Error())
 	}
