0b97add1fd9ae0c89afe0b4e1160f1d7a93c12a9
Aaron Leung <aaron.leung@moovweb.com>
2012-04-25 17:55:35 -0700

    Commenting out the Hammacher test until we're using the custom icu4c library.

--- detect_test.go
+++ detect_test.go
@@ -35,21 +35,22 @@ func TestDetectLatin1(t *testing.T) {
 	detector.Free()
 }
 
-func TestDetectHammacher(t *testing.T) {
-	data, err := ioutil.ReadFile("hammacher.html")
-	if err != nil {
-		t.Errorf("error: %s", err.Error())
-	}
-	detector, err := NewCharsetDetector()
-	if err != nil {
-		t.Errorf("error: %s", err.Error())
-	}
-	charset := detector.GuessCharset(data)
-	if charset != "ISO-8859-1" {
-		t.Errorf("the charset guess was wrong")
-	}
-	detector.Free()
-}
+// The following commented test requires our custom build of the icu4c lib.
+// func TestDetectHammacher(t *testing.T) {
+//  data, err := ioutil.ReadFile("hammacher.html")
+//  if err != nil {
+//    t.Errorf("error: %s", err.Error())
+//  }
+//  detector, err := NewCharsetDetector()
+//  if err != nil {
+//    t.Errorf("error: %s", err.Error())
+//  }
+//  charset := detector.GuessCharset(data)
+//  if charset != "ISO-8859-1" {
+//    t.Errorf("the charset guess was wrong")
+//  }
+//  detector.Free()
+// }
 
 func _TestDetectCp1252(t *testing.T) {
 	data, err := ioutil.ReadFile("input_cp1252.html")
